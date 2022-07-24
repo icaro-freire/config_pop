8da0c352e0e0868b585f206fb50ecf47137aa384
icaro-freire <icarofreire7@gmail.com>
2022-07-24 16:27:06 -0300

    dev: adicionei comando para PATH/HOME/bin

--- funcoes/adicionarBin.sh
+++ funcoes/adicionarBin.sh
@@ -4,8 +4,10 @@
 adicionar_bin () {
   echo 
   echo "##### Copiando alguns scripts pessoais para diretóio bin, na HOME =============="
+  echo "      E tornando o diretóio bin um caminho no PATH"
   echo 
   cp ./funcoes/bin $HOME -r
+  PATH=$PATH:$HOME/bin
   echo "----- FEITO (14/ $TOTAL) -----------------------------------------------------------"
 }
 
